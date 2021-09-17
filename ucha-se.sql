eecad5157903d2cf88a22754a4022c5bfaac5284
Emanuil Karapachov <406743@student.fontys.nl>
2021-09-17 22:44:38 +0200

    Release1.3

--- ucha-se.sql
+++ ucha-se.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 17, 2021 at 10:33 PM
+-- Generation Time: Sep 17, 2021 at 10:43 PM
 -- Server version: 10.4.16-MariaDB
 -- PHP Version: 7.4.12
 
